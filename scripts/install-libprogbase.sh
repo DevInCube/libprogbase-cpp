3674dbb5c08a735915609eacfff7b1669c08d0d4
PublicHadyniak <public.hadyniak@gmail.com>
2017-04-23 17:23:49 +0300

    update libprogbase install script

--- scripts/install-libprogbase.sh
+++ scripts/install-libprogbase.sh
@@ -2,5 +2,5 @@
 
 wget -O libprogbase.zip https://github.com/PublicHadyniak/libprogbase/archive/master.zip
 unzip libprogbase.zip -d libprogbase
-cd libprogbase && cd libprogbase-master && sudo make install
-cd ../../ && rm libprogbase/ -rf && rm libprogbase.zip
+cd libprogbase ; cd libprogbase-master ; sudo make install
+cd ../../ ; rm libprogbase/ -rf ; rm libprogbase.zip
